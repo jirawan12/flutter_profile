fff546422e462a18082f6537ec333800821b2f05
unknown <jirawan.boon61@sskru.ac.th>
2020-12-03 18:29:04 +0700

    asdasd

---
+++ lib/page2.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyPage2 extends StatelessWidget {
+  MyPage2({Key key}) : super(key: key);
+
+  var sizeBox = 14.0;
+
+  TextStyle bulletStyle = GoogleFonts.prompt(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+  );
+
+  TextStyle contentBulletStyle = GoogleFonts.prompt(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  TextStyle contentStyle = GoogleFonts.kanit(
+    fontSize: 18,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'My Alabum',
+          style: contentStyle,
+        ),
+        automaticallyImplyLeading: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+              child: Column(
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Center(
+                      child: Text(
+                        "ถนอมวรรณ จิราวรรณ",
+                        style: bulletStyle,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50.0),
+                    child: Image.asset(
+                      'assets/imges/5.jpg',
+                      height: 380.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50.0),
+                    child: Image.asset(
+                      'assets/imges/4.jpg',
+                      height: 500.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50.0),
+                    child: Image.asset(
+                      'assets/imges/3.jpg',
+                      height: 480.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50.0),
+                    child: Image.asset(
+                      'assets/imges/2.jpg',
+                      height: 480.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50.0),
+                    child: Image.asset(
+                      'assets/imges/1.jpg',
+                      height: 480.0,
+                    ),
+                  ),
+                  SizedBox(height: sizeBox),
+                  ListTile(
+                    title: Align(
+                      alignment: Alignment(-1, 0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
